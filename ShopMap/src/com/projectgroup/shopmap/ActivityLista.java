3d9836605d4720516f957533257e40ba1d85786f
mau91 <maurizioabaterusso@gmail.com>
2015-06-17 15:52:21 +0200

    provaLista

--- ShopMap/src/com/projectgroup/shopmap/ActivityLista.java
+++ ShopMap/src/com/projectgroup/shopmap/ActivityLista.java
@@ -1,34 +1,52 @@
 package com.projectgroup.shopmap;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class ActivityLista extends Activity {
 
+	ListAdapter listAdapter;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_lista);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_lista, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+		
+		ArrayList<String> shopList = new ArrayList<String>();
+		final ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1);
+        final ListView shopM= (ListView)findViewById(R.id.list_selection);
+        shopM.setAdapter(listAdapter);
+        
+        ArrayList<ParseObject> result = ShopMapApplication.getShopsByLocation(location user);
+        
+        for(ParseObject object: result) {
+                         String nome = object.getString("nome_attivita").toString();
+                         String categoria_secondaria = object.getString("categoria_secondaria").toString();
+                         String distanza = String.valueOf(object.getDouble("distanza"));
+                         
+                          listAdapter.add(nome);
+                          listAdapter.add(categoria_secondaria);
+                          listAdapter.add(distanza);
+                
+                
+        			}
+        }
+         
+		
 }
+
+
+
+
