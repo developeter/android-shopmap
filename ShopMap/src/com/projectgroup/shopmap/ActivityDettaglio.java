00965fc3b5aa5dcd5a76639628b33645f492a0df
mau91 <maurizioabaterusso@gmail.com>
2015-06-26 15:17:34 +0200

    avanzamento activity

--- ShopMap/src/com/projectgroup/shopmap/ActivityDettaglio.java
+++ ShopMap/src/com/projectgroup/shopmap/ActivityDettaglio.java
@@ -1,19 +1,81 @@
 package com.projectgroup.shopmap;
 
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ActivityDettaglio extends Activity {
+
+	TextView cat_princ;
+	TextView cat_sec;
+	TextView nome_etichetta;
+	TextView descr;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_dettaglio);
+		Bundle bundle = getIntent().getExtras();
+
+		cat_princ = (TextView)findViewById(R.id.cat_princ);
+		cat_sec = (TextView)findViewById(R.id.cat_sec);
+		nome_etichetta = (TextView)findViewById(R.id.nome);
+		descr = (TextView)findViewById(R.id.descrizione);
+
+		final String nome_string = (String) bundle.get("NOME");
+		
+		if(bundle != null){
+			getDetailById(nome_string);
+		}
+
+		Button ButtonGallery = (Button) findViewById(R.id.btn_gallery);
+		/*ButtonGallery.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View view) {
+				Bundle newbundle = new Bundle();
+				newbundle.putString("NOME", nome_string);
+				Intent gal = new Intent(ActivityDettaglio.this, GalleryActivity.class);
+				gal.putExtras(newbundle);
+				startActivity(gal);
+
+			}
+		}); */
+
 	}
+	private void getDetailById(final String nome){
+		
+		ParseQuery<ParseObject> query = ParseQuery.getQuery("attivita");
+		query.whereEqualTo("nome_attivita", nome);
+		query.fromLocalDatastore();
+		query.getFirstInBackground(new GetCallback<ParseObject>() {
+			public void done(ParseObject object, ParseException e) {
+				if (e == null) {
 
+					try {
+						object.pin();
+					} catch (ParseException e1) {
+						Log.d("errore", "Impossibile salvare sul database: " + e1.getMessage());
+					}
+					
+					cat_princ.setText(object.getString("categoria_principale"));
+					cat_sec.setText(object.getString("categoria_secondaria"));
+					nome_etichetta.setText(nome);
+					descr.setText(object.getString("descrizione"));
 
-
-
+				} else {
+					Log.d("errore", "Errore nel download: " + e.getMessage());
+				}
+			}
+		});
+	}
 }
--- ShopMap/src/com/projectgroup/shopmap/ListAdapter.java
+++ ShopMap/src/com/projectgroup/shopmap/ListAdapter.java
@@ -2,6 +2,7 @@ package com.projectgroup.shopmap;
 
 import java.util.ArrayList;
 
+import android.app.Activity;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,46 +10,28 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-import com.parse.ParseObject;
+public class ListAdapter extends ArrayAdapter<String[]>{
 
-public class ListAdapter extends ArrayAdapter<ParseObject>{
-
-	private final Context context;
-	private final ArrayList<ParseObject> list;
+	private final Activity context;
 	
-	public ListAdapter(Context context, ArrayList<ParseObject> list){
-		super(context, R.layout.cella, list);
+	public ListAdapter(Activity context, ArrayList<String[]> contenuto){
+		super(context, R.layout.cella, contenuto);
 		this.context=context;
-		this.list=list;
 	}
 	
 	public View getView(int position, View view, ViewGroup parent) {
-		LayoutInflater inflater = (LayoutInflater)context.getSystemService
-			      (Context.LAYOUT_INFLATER_SERVICE);
-		View rowView=inflater.inflate(R.layout.cella, null, true);
+		LayoutInflater inflater = (LayoutInflater)context.getSystemService (Context.LAYOUT_INFLATER_SERVICE);
+		View rowView = inflater.inflate(R.layout.cella, null, true);
 		
-		TextView nome = (TextView)rowView.findViewById(R.id.nome);
-		TextView cat = (TextView)rowView.findViewById(R.id.cat);
-		TextView dist = (TextView)rowView.findViewById(R.id.dist);
+		TextView nome = (TextView) rowView.findViewById(R.id.nome);
+		TextView cat = (TextView) rowView.findViewById(R.id.cat);
+		TextView dist = (TextView) rowView.findViewById(R.id.dist);
 	
-		String[] data = new String[3];
-		data = extractFromList(position);
-		
-		nome.setText(data[1]);
-		cat.setText(data[2]);
-		dist.setText(data[3]);
+		String[] attivita = this.getItem(position);
+		nome.setText(attivita[0]);
+		cat.setText(attivita[1]);
+		dist.setText(attivita[2]);
 		
 		return rowView;
-	}
-	
-	private String[] extractFromList(int position){
-		
-		ParseObject object = list.get(position);
-		String[] data = new String[3];
-		data[1] = object.getString("nome_attivita");
-		data[2] = object.getString("categoria_secondaria");
-		data[3] = object.getString("distanza");
-		
-		return data;
 	}
 }
