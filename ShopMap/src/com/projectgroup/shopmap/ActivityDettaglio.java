c4eb5bdce7d9d0616ba0373aba57353be9e82af3
mau91 <maurizioabaterusso@gmail.com>
2015-07-03 15:20:52 +0200

    material design-navigation drawer

--- ShopMap/src/com/projectgroup/shopmap/ActivityDettaglio.java
+++ ShopMap/src/com/projectgroup/shopmap/ActivityDettaglio.java
@@ -1,19 +1,18 @@
 package com.projectgroup.shopmap;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
-public class ActivityDettaglio extends Activity {
+public class ActivityDettaglio extends AppCompatActivity {
 
 	TextView cat_princ;
 	TextView cat_sec;
@@ -26,34 +25,35 @@ public class ActivityDettaglio extends Activity {
 		setContentView(R.layout.activity_dettaglio);
 		Bundle bundle = getIntent().getExtras();
 
-		cat_princ = (TextView)findViewById(R.id.cat_princ);
-		cat_sec = (TextView)findViewById(R.id.cat_sec);
-		nome_etichetta = (TextView)findViewById(R.id.nome);
-		descr = (TextView)findViewById(R.id.descrizione);
+		cat_princ = (TextView) findViewById(R.id.cat_princ);
+		cat_sec = (TextView) findViewById(R.id.cat_sec);
+		nome_etichetta = (TextView) findViewById(R.id.nome);
+		descr = (TextView) findViewById(R.id.descrizione);
 
 		final String nome_string = (String) bundle.get("NOME");
-		
-		if(bundle != null){
+
+		if (nome_string != null) {
 			getDetailById(nome_string);
+		} else {
+			Toast.makeText(this, "Bundel vuoto", Toast.LENGTH_SHORT).show();
 		}
 
-		Button ButtonGallery = (Button) findViewById(R.id.btn_gallery);
-		/*ButtonGallery.setOnClickListener(new View.OnClickListener() {
-
-			@Override
-			public void onClick(View view) {
-				Bundle newbundle = new Bundle();
-				newbundle.putString("NOME", nome_string);
-				Intent gal = new Intent(ActivityDettaglio.this, GalleryActivity.class);
-				gal.putExtras(newbundle);
-				startActivity(gal);
-
-			}
-		}); */
+		// Button ButtonGallery = (Button) findViewById(R.id.btn_gallery);
+		/*
+		 * ButtonGallery.setOnClickListener(new View.OnClickListener() {
+		 * 
+		 * @Override public void onClick(View view) { Bundle newbundle = new
+		 * Bundle(); newbundle.putString("NOME", nome_string); Intent gal = new
+		 * Intent(ActivityDettaglio.this, GalleryActivity.class);
+		 * gal.putExtras(newbundle); startActivity(gal);
+		 * 
+		 * } });
+		 */
 
 	}
-	private void getDetailById(final String nome){
-		
+
+	private void getDetailById(final String nome) {
+
 		ParseQuery<ParseObject> query = ParseQuery.getQuery("attivita");
 		query.whereEqualTo("nome_attivita", nome);
 		query.fromLocalDatastore();
@@ -64,9 +64,10 @@ public class ActivityDettaglio extends Activity {
 					try {
 						object.pin();
 					} catch (ParseException e1) {
-						Log.d("errore", "Impossibile salvare sul database: " + e1.getMessage());
+						Log.d("errore", "Impossibile salvare sul database: "
+								+ e1.getMessage());
 					}
-					
+
 					cat_princ.setText(object.getString("categoria_principale"));
 					cat_sec.setText(object.getString("categoria_secondaria"));
 					nome_etichetta.setText(nome);
--- ShopMap/src/com/projectgroup/shopmap/MainActivity.java
+++ ShopMap/src/com/projectgroup/shopmap/MainActivity.java
@@ -1,84 +1,108 @@
 package com.projectgroup.shopmap;
 
-import java.util.List;
-
-import com.parse.FindCallback;
-import com.parse.ParseException;
-import com.parse.ParseObject;
-import com.parse.ParseQuery;
-
 import android.app.Activity;
 import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends Activity{
-	
-	
+	DrawerLayout mDrawerLayout;
+	NavigationView navigation;
 	private int listsize = 0;
 	private TextView testText;
-	private Button GoToMaps;
-	private Button gallery;
-	private Button lista;
+	private Button GoToMaps, GoToList;
+	private Location lastLocation;
+	private Location currentLocation;
+	private final String USER_LOCATION = "user_location";
+	private final String USER_DATA = "user_data";
+
+	// private final Map<String, Marker> mapMarkers = new HashMap<String,
+	// Marker>();
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		testText = (TextView)findViewById(R.id.textTest);
-		GoToMaps = (Button)findViewById(R.id.btnOpenMap);
-		lista = (Button)findViewById(R.id.btn_lista);
+		 final ActionBar actionBar = getSupportActionBar();
+		    actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
+		    actionBar.setDisplayHomeAsUpEnabled(true);
+		    
+		
+
+		GoToMaps = (Button) findViewById(R.id.btnOpenMap);
+		GoToList = (Button) findViewById(R.id.btnOpenList);
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.navigation);
 		
 		GoToMaps.setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
-				Intent add = new Intent(MainActivity.this, MapActivity.class);
-				startActivity(add);				
+				Intent inMapAct = new Intent(MainActivity.this,MapActivity.class);
+				startActivity(inMapAct);
+			}
+		});
+
+		GoToList.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent inListAct = new Intent(MainActivity.this,
+						ActivityLista.class);
+
+				startActivity(inListAct);
 			}
 		});
 		
-		ParseQuery<ParseObject> query = ParseQuery.getQuery("attivita");
-		query.whereEqualTo("categoria_principale", "negozio");
-		query.findInBackground(new FindCallback<ParseObject>() {
-			
-			public void done(List<ParseObject> listNegozi, ParseException e) {
-		        if (e == null) {
-		        	listsize = listNegozi.size();
-		        	setText(listsize);
-		        } else {
-		            Log.d("score", "Error: " + e.getMessage());
-		        }
-		    }
-		});
-		
-		gallery = (Button) findViewById(R.id.btn_gallery);
-		gallery.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				Intent gal = new Intent(MainActivity.this, GalleryActivity.class);
-				startActivity(gal);				
-			}
-		});
-		
-		lista.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				
-				Intent open = new Intent(MainActivity.this, ActivityLista.class);
-				startActivity(open);
-			}
-		});
+		navigationSlide();
 	}
 	
-	public void setText(int value){	
-		testText.setText(""+value);
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    switch (item.getItemId()) {
+	        case android.R.id.home:
+	            mDrawerLayout.openDrawer(GravityCompat.START);
+	            return true;
+	    }
+	    return super.onOptionsItemSelected(item);
+	}
+	
+	private void navigationSlide() {
+		navigation = (NavigationView) findViewById(R.id.navigation_d);
+	    navigation.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+			@Override
+			public boolean onNavigationItemSelected(MenuItem menuItem) {
+				int id = menuItem.getItemId();
+	            switch (id) {
+	                case R.id.navItem1:
+	                	Intent inMapAct = new Intent(MainActivity.this,MapActivity.class);
+	    				startActivity(inMapAct);
+	                    break;
+	                case R.id.navItem2:
+	                	Intent inListAct = new Intent(MainActivity.this,ActivityLista.class);
+
+	    				startActivity(inListAct);
+	                    break;
+	            }
+	            mDrawerLayout.closeDrawers();
+				return false;
+			}
+	    });
+	}
+	
+	public void setText(int value) {
+
+		testText.setText("" + value);
 	}
 
 }
